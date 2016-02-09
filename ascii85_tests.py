48c4d832e43744ae1d204d1554acd1f3b8c24e81
Jack Carlson <carlsonjack@gmail.com>
2016-02-09 16:16:04 -0600

    Create ascii85_tests.py

---
+++ ascii85_tests.py
@@ -0,0 +1,6 @@
+Test.assert_equals(toAscii85('easy'), '<~ARTY*~>')
+Test.assert_equals(toAscii85('moderate'), "<~D/WrrEaa'$~>")
+Test.assert_equals(toAscii85('somewhat difficult'), '<~F)Po,GA(E,+Co1uAnbatCif~>')
+
+Test.assert_equals(fromAscii85('<~ARTY*~>'), 'easy')
+Test.assert_equals(fromAscii85('<~F)Po,GA(E,+Co1uAnbatCif~>'), 'somewhat difficult')
